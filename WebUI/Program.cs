12ce34f9ee20ad9e82d59bf48824d92d6b842298
Hamed sharafdoost <nt92481@gmail.com>
2023-01-29 23:19:51 +0330

    Complete implementing repositories

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,7 +1,23 @@
+using ApplicationCore.Interfaces;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<MainContext, MainContext>();
+//builder.Services.AddDbContext<MainContext>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen(v => v.SwaggerDoc("v1", new OpenApiInfo
+{
+    Title = "DefaultApiTemplate",
+    Version = "v1"
+}));
+
 
 var app = builder.Build();
 
@@ -11,14 +27,18 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint(
+"/swagger/v1/swagger.json", "DefaultApiTemplate v1"));
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.MapControllerRoute(
     name: "default",
