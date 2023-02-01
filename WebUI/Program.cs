dd1a4570c87f7e8f95fd314beb2e3ff73dbd809c
Hamed sharafdoost <nt92481@gmail.com>
2023-02-01 22:26:33 +0330

    implement Icomment repository interface

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,15 +1,17 @@
 using ApplicationCore.Interfaces;
 using Infrastructure;
 using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-//builder.Services.AddScoped<IProductRepository, ProductRepository>();
-//builder.Services.AddScoped<IUserRepository, UserRepository>();
-//builder.Services.AddScoped<MainContext, MainContext>();
-//builder.Services.AddDbContext<MainContext>();
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddDbContext<MainContext>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen(v => v.SwaggerDoc("v1", new OpenApiInfo
@@ -27,7 +29,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-
 }
 
 app.UseHttpsRedirection();
